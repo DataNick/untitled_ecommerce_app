d3c99c06cdce43104bf9b8c02588fdf6335abf6a
DataNick <DataNick@users.noreply.github.com>
2016-02-25 13:30:19 -0500

    Specified order

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,6 +1,7 @@
 class OrderMailer < ActionMailer::Base
   default from: "@gmail.com"
   def order_confirmation(order)
+    @order = order
     mail to: order.user.email, subject: "Your order (##{order.id})"
   end
 end
