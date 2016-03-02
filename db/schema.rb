2daedce81065069188a3a0073e8557cb33522ac6
DataNick <DataNick@users.noreply.github.com>
2016-03-02 17:18:49 -0500

    Migrated database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160223154911) do
+ActiveRecord::Schema.define(version: 20160302221547) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "order_items", force: :cascade do |t|
     t.integer  "order_id"
